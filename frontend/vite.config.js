e5ff271c51dc7000b291fdced03f421e151dec75
Jorge Zamora <jorge.zamora245103@potros.itson.edu.mx>
2025-11-08 13:34:22 -0700

    Cambio

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/https://pazoomer.github.io/FrontendAdministracionSorteos/',
+  base: '/FrontendAdministracionSorteos/',  // solo la subcarpeta del repo
 })
